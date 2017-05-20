ae8f7dd30c5566df43995876d8e8a2cabc9bb851
lachlan <lachlan.miller.1990@outlook.com>
2017-05-20 10:14:51 +0900

    Improve query

--- app/controllers/japanese_words_controller.rb
+++ app/controllers/japanese_words_controller.rb
@@ -1,47 +1,29 @@
 class JapaneseWordsController < ApplicationController
-  def words_by_radicals
-    search_terms = params[:radicals]
-    query = ""
-    search_terms.each_with_index do |radical, idx| 
-      query << "radicals LIKE '%#{radical}%'"
-      query << " AND " unless idx + 1 == search_terms.length
-    end
-
-    words = JapaneseWord.where(query)
-
-    json_response(words)
-  end
-
   def meaning_by_kanji_and_radicals
-    radicals = params[:radicals]
-    kanji = params[:kanji]
+    radicals = params[:radicals].reject { |c| c.empty? }
+    kanji = params[:kanji].reject { |c| c.empty? }
     length = params[:length]
-    p length
-    radicalQuery = ""
-    radicals.each_with_index do |rad, idx|
 
-      radicalQuery << "radicals LIKE '%#{rad}%'" 
-      radicalQuery << " AND " unless idx + 1 == radicals.length
+    radQuery = ""
+    radicals.each_with_index do |rad, idx| 
+      if (rad.length > 0)
+        radQuery << "radicals LIKE '%#{rad}%'"
+        radQuery  << " AND " unless idx + 1 == radicals.length
+      end
     end
 
     wordQuery = ""
     kanji.each_with_index do |kan, idx| 
-      wordQuery << "word LIKE '%#{kan}%'"
+      wordQuery << "(word LIKE '%#{kan}%' or hiragana LIKE '%#{kan}%')"
       wordQuery << " AND " unless idx + 1 == kanji.length
     end
 
-    query = ""
+    lenQuery = ""
     if radicals.length > 0 && kanji.length > 0
-      query = radicalQuery << " AND " << wordQuery
-    elsif radicalQuery.length == 0 && kanji.length > 0
-      query = wordQuery
-    else
-      query = radicalQuery
+      lenQuery << "length(japanese_words.word) = #{length}"
     end
 
-    query << " AND length(japanese_words.word) = #{length}"
-    words = JapaneseWord.where(query)
-
+    words = JapaneseWord.where(wordQuery).where(radQuery).where(lenQuery)
     json_response(words)
   end
 
--- app/models/japanese_word.rb
+++ app/models/japanese_word.rb
@@ -1,2 +1,3 @@
 class JapaneseWord < ApplicationRecord
+
 end
